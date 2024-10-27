95ce0bbbf10a33269a843b3edc84b0f908a0e7cf
Crumbobly <vkvalera004@gmail.com>
2024-10-27 16:25:20 +0300

    .

--- server.gd
+++ server.gd
@@ -8,7 +8,7 @@ func create_server():
 	var server = ENetMultiplayerPeer.new()
 	server.create_server(12345)
 	multiplayer.multiplayer_peer = server
-	print("server created")
+	print_debug("server created")
 	
 	
 	multiplayer.peer_connected.connect(_on_player_connected)
@@ -16,10 +16,10 @@ func create_server():
 
 
 func _on_player_connected(peer_id):
-	print("Игрок подключился с ID:", peer_id)
+	print_debug("Игрок подключился с ID:", peer_id)
 	# Добавь логику для инициализации данных для нового игрока
 
 func _on_player_disconnected(peer_id):
-	print("Игрок отключился с ID:", peer_id)
+	print_debug("Игрок отключился с ID:", peer_id)
 	# Убери игрока из игры или обработай его отключение
 	
